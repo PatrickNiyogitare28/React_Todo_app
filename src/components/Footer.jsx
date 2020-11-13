e63f0ac780e1c7c9318fb692d60dfa32f2cf2198
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2020-11-13 11:02:35 +0200

    fit added tooltips

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import ReactTooltip from 'react-tooltip';
+
 import '../styles/footer.css';
 import checkLogo from '../assets/images/check-icon.png';
 import {Link} from 'react-router-dom';
@@ -16,14 +18,17 @@ export default function Footer(propos) {
                               toDO
                            </li>
                            <li title="Go to Home">
-                              <Link to="/">{propos.page}</Link> 
+                              <Link to="/" data-tip="Go Home">{propos.page}</Link> 
                            </li>
                        </ul>
                     </div>
                     <div className="right-wrapper">
                          <ul>
                              <li>
-                               Copyright 2020, <a href="https://patrickniyo.netlify.app" target="_blank">PatrickNiyo</a>
+                               Copyright 2020, 
+                               <a href="https://patrickniyo.netlify.app" target="_blank" data-tip="Visit Developer's official website">PatrickNiyo</a>
+                               <ReactTooltip place="left" type="info" effect="solid"/>
+
                             </li>
                          </ul>
                     </div>
